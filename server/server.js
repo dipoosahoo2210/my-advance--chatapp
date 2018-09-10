48d4685da285c6c9e79ca564ea5fb5b3e9124e14
smarteye <pakaudirector@gmail.com>
2018-09-10 15:07:43 +0530

    Init commit

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const  path = require('path');
+const express = require('express');
+
+
+const publicpath = path.join(__dirname,'../public');
+var port = process.env.PORT||3000;
+
+var app = express();
+
+app.use(express.static(publicpath));
+
+app.listen(port,()=>{
+    console.log(`app is listen to  port : ${port}`);
+})
