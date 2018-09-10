9cba0b508ff33732f4066ca84235e13f5eda72ea
smarteye <pakaudirector@gmail.com>
2018-09-10 19:53:40 +0530

    chage files

--- server/server.js
+++ server/server.js
@@ -1,12 +1,11 @@
-const  path = require('path');
-const  http = require('http');
+const path = require('path');
+const http = require('http');
 const express = require('express');
-const  socketIO = require('socket.io');
+const socketIO = require('socket.io');
 
 
-
-const publicpath = path.join(__dirname,'../public');
-var port = process.env.PORT||3000;
+const publicpath = path.join(__dirname, '../public');
+var port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -16,32 +15,37 @@ var io = socketIO(server);
 
 app.use(express.static(publicpath));
 
-
-io.on("connection" ,(socket)=>{
-    console.log(" a  new user is connected");
-
-
+   var count = 1;
+io.on("connection", (socket) => {
+    console.log(" a  new user is connected" , count++);
 
 
-    socket.emit('newEmail' ,{
-        from:"pakaudirector@gmail.com",
-        text:" hey whats goin on",
+    socket.emit('newEmail', {
+        from: "pakaudirector@gmail.com",
+        text: " hey whats goin on",
         createat: 1234
     });
-    socket.on('createMessage',(message)=>{
+    socket.on('createMessage', (message) => {
         console.log("createMessage :", message);
+
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            textat:new Date().getTime()
+
+        })
     })
 
-    socket.emit('newMessage' ,{
-        from:"reena rani sahoo",
-        text:"meet you soon",
-        createat:122344
+    socket.emit('newMessage', {
+        from: "reena rani sahoo",
+        text: "meet you soon",
+        createat: 122344
     })
 
-    socket.on('disconnect',()=>{
+    socket.on('disconnect', () => {
         console.log("clint was disconnected");
     })
 })
-server.listen(port,()=>{
+server.listen(port, () => {
     console.log(`app is listen to  port : ${port}`);
 })
